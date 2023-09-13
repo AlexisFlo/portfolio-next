1e2724073d13de2f3a0165a1ee3b14a211af33d4
Flowers <floresfernandezalexis05@gmail.com>
2023-09-13 17:46:31 -0600

    feat: Create dynamic Navbar component
    - Added a Navbar component that dynamically renders navigation links based on the 'navItems' object.
    - Used window.location.pathname to determine the current pathname on the client-side.
    - Applied CSS classes and animations for link styling and active states.

---
+++ app/components/sidebar.js
@@ -0,0 +1,73 @@
+'use client'
+
+import Link from 'next/link';
+import { LayoutGroup, motion } from 'framer-motion';
+
+const navItems = {
+  '/': {
+    name: 'home',
+  },
+  '/projects': {
+    name: 'projects',
+  },
+  '/guestbook': {
+    name: 'guestbook',
+  },
+};
+
+export default function Navbar() {
+  // Get the current pathname, using window.location.pathname on the client-side,
+  // or fallback to '/' if running on the server-side where window is undefined.
+  const pathname = typeof window !== 'undefined' ? window.location.pathname : '/';
+
+  return (
+    <aside className="-ml-[8px] mb-16 tracking-tight">
+      <div className="lg:sticky lg:top-20">
+        <LayoutGroup>
+          <nav
+            className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+            id="nav"
+          >
+            {/* Render navigation links dynamically based on 'navItems' object.
+              - 'path' is compared with 'pathname' to determine if a link is active.
+              - 'linkClasses' is dynamically generated based on the link's state.
+              - 'motion.div' is used for an animation effect when the link is active.
+            */}
+            <div className="flex flex-row space-x-0 pr-10">
+              {Object.entries(navItems).map(([path, { name }]) => {
+                const isActive = path === pathname;
+                const linkClasses = [
+                  'transition-all hover:text-neutral-800 dark:hover:text-neutral-200',
+                  'flex align-middle',
+                  isActive ? 'text-neutral-500' : '',
+                ].filter(Boolean).join(' ');
+
+                return (
+                  <Link legacyBehavior 
+                    key={path} href={path}>
+                    <a className={linkClasses}>
+                      <span className="relative py-1 px-2">
+                        {name}
+                        {path === pathname ? (
+                          <motion.div
+                            className="absolute h-[1px] top-7 mx-2 inset-0 bg-neutral-200 dark:bg-neutral-800 z-[-1] dark:bg-gradient-to-r from-transparent to-neutral-900"
+                            layoutId="sidebar"
+                            transition={{
+                              type: 'spring',
+                              stiffness: 350,
+                              damping: 30,
+                            }}
+                          />
+                        ) : null}
+                      </span>
+                    </a>
+                  </Link>
+                );
+              })}
+            </div>
+          </nav>
+        </LayoutGroup>
+      </div>
+    </aside>
+  );
+}
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
+import Sidebar from './components/sidebar';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
+        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+          <Sidebar />
+          {children}
+        </main>
+        </body>|
     </html>
   )
 }
